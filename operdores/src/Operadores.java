22946483b72ccd40a7938fd92a3c5ba99fcc5028
karlos marques de menezes <103792595+karlosmarques@users.noreply.github.com>
2025-04-03 08:36:32 -0300

    Update Operadores.java

--- operdores/src/Operadores.java
+++ operdores/src/Operadores.java
@@ -19,5 +19,64 @@ public class Operadores {
 
         System.out.println(variavel);
 
+
+
+        //operador ternario:
+
+        int a, b;
+
+        a= 5;
+        b= 6;
+
+        String resultado = "";
+        if (a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+
+        System.out.println(resultado);
+
+
+        //operadores relacionais:
+
+        String nomeUm = "KARLOS";
+        String nomeDois = new String("KARLOS");
+
+        System.out.println(nomeUm.equals(nomeDois));
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+        
+        boolean simNao = numero1 == numero2;
+
+
+        if (numero1 == numero2) {
+            System.out.println("a nossa condição é verdadiera");
+        }
+
+        System.out.println("numeroUm é igual a numeroDois" + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente a numeroDois" + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior que numeroDois" + simNao);
+
+
+        //operadores logicos
+
+         boolean condicao1 = true;
+
+         boolean condicao2 = false;
+         if (condicao1 && condicao2){
+            System.out.println("as duas condições são verdadeiras");
+         }
+
+         if (condicao1 || condicao2){
+            System.out.println("uma das condições e verdadeira");
+         }
     }
 }
