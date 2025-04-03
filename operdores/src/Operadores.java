96e562502de78d8ca57dfad1926a29024b65cf02
karlos marques de menezes <103792595+karlosmarques@users.noreply.github.com>
2025-04-03 08:06:40 -0300

    operadores

---
+++ operdores/src/Operadores.java
@@ -0,0 +1,23 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        int numero = 5;
+
+        // x reteticao
+
+        //numero = numero + 1
+
+        System.out.println(numero --);
+
+        System.out.println(numero);
+
+
+
+
+        boolean variavel = true;
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+
+    }
+}
