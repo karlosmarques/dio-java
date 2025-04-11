ee1f3a69af3cbf224fb6bd8010d1eb87bb701025
karlos marques de menezes <103792595+karlosmarques@users.noreply.github.com>
2025-04-11 10:38:33 -0300

    Update exforarry.java

--- controle-de-fluxo/src/exforarry.java
+++ controle-de-fluxo/src/exforarry.java
@@ -1,6 +1,6 @@
 public class exforarry {
     public static void main(String[] args) {
-        String [] alunos = {"KARLOS", "DAFNI","ALANA","BRENDA"};
+        String [] alunos = {"lucas", "ewerton","karlos","mel","carol"};
         for (int i = 0; i < alunos.length; i++) {
             System.out.println("O aluno no indice x=" + i + " é " + alunos[i]);
         }
